74c473b7c66f288307b76b11df5766d94eba2882
Gabriel <gabrielfg12345@gmail.com>
2025-04-01 16:32:50 -0300

    adicionando alterações e expandindo código

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
         System.out.println("Que lindo dia!");
+        int alunos;
+        alunos = 512;
+        int salas;
+        salas = 24;
+        System.out.println("A quantidade de alunos por sala é: " + (alunos) / (salas));
     }
 }
