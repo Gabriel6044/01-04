158d8a78152d4ae1b9f6f281d6e7906bd6eb12ef
Gabriel <gabrielfg12345@gmail.com>
2025-04-01 18:03:34 -0300

    alteração final 01-04

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-        System.out.println("Que lindo dia!");
-        int alunos;
-        alunos = 512;
+        System.out.println("Olá, Boa tarde!");
+        System.out.println("Aqui está o resultado da divisão de alunos por sala:");
+        int alunos = 513;
         int salas;
         salas = 24;
-        System.out.println("A quantidade de alunos por sala é: " + (alunos) / (salas));
+        int divisao;
+        divisao = (alunos) / (salas);
+        System.out.println("A quantidade de alunos por sala é: " + (divisao));
+        System.out.println("Alunos restantes: " + ((alunos) - ((divisao) * (salas))));
     }
 }
