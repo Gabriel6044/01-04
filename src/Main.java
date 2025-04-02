4646cec10e98e7ac6c30ede92ac176f280b0ed0b
Gabriel <gabrielfg12345@gmail.com>
2025-04-02 17:53:06 -0300

    ---------------------- ---------------------- Ordem-Servico: SGPS-#9999999

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        int alunos = 64;
+        int salas = 10;
+        int divisao  = (alunos) / (salas);
+        int resto = (alunos) % (salas);
         System.out.println("Olá, Boa tarde!");
-        System.out.println("Aqui está o resultado da divisão de alunos por sala:");
-        int alunos = 512;
-        int salas;
-        salas = 24;
-        int divisao;
-        divisao = (alunos) / (salas);
+        System.out.println("Há " + (alunos) + " alunos e " + (salas) + " salas.");
+        System.out.println("Aqui está o resultado da divisão igualitária por sala:");
+
         System.out.println("A quantidade de alunos por sala é: " + (divisao));
-        System.out.println("Alunos restantes: " + ((alunos) - ((divisao) * (salas))));
+        System.out.println();
+
+        switch (resto) {
+            case 0:
+                System.out.println("Não há alunos sobrando.");
+                break;
+            case 1:
+                System.out.println("Há " + (resto) + " aluno sobrando.");
+                break;
+            default:
+                System.out.println("Há " + (resto) + " alunos sobrando.");
+                break;
+        }
+
     }
 }
